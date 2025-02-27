e1be4b97d36a6783a9e27fdc0029982e6ec319d2
EstebanCaponio <estebancaponio@gmail.com>
2025-02-27 18:15:36 +0100

    fix: fixed string

--- dadi/dadi.js
+++ dadi/dadi.js
@@ -3,7 +3,7 @@ let computerRandom = Math.floor(Math.random() * (6) + 1);
 console.log(`computer: n.${computerRandom}`);
 //genero un numero random per il giocatore e lo stampo
 let userRandom = Math.floor(Math.random() * (6) + 1);
-console.log(`computer: n.${userRandom}`);
+console.log(`Player 1: n.${userRandom}`);
 //creo un if dove stabilisco il vincitore o se si verifica un pareggio. 
  if (computerRandom > userRandom){
     console.log(`Il computer ha vinto.`);
