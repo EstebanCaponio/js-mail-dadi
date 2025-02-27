0ddd7b6326ef1ee163c29aafdaa8fd4653c89cf3
EstebanCaponio <estebancaponio@gmail.com>
2025-02-27 18:13:16 +0100

    feat: finish 2nd exercise

---
+++ dadi/dadi.js
@@ -0,0 +1,13 @@
+//genero un numero random per il computer e lo stampo
+let computerRandom = Math.floor(Math.random() * (6) + 1);
+console.log(`computer: n.${computerRandom}`);
+//genero un numero random per il giocatore e lo stampo
+let userRandom = Math.floor(Math.random() * (6) + 1);
+console.log(`computer: n.${userRandom}`);
+//creo un if dove stabilisco il vincitore o se si verifica un pareggio. 
+ if (computerRandom > userRandom){
+    console.log(`Il computer ha vinto.`);
+ } else if (computerRandom < userRandom){
+    console.log(`Hai vinto.`);
+ } else
+ console.log(`Pareggio.`);
